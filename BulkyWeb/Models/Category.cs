6a48ce2e5e0e125cb18f286448f5cbb7a869c490
LIBCSYS <libcsystems@gmail.com>
2024-01-23 19:01:37 -0500

    Break Point **** Good Progress Made. Database Created. Added new .cshtml page named Category List.

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * 
+ * category.cs - will connect to the database
+ * 
+ * 
+ */
+using System.ComponentModel.DataAnnotations;
+    
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } 
+        public int DisplayOrder { get; set; }
+
+    }
+}
